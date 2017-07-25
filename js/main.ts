443030ae1dfcd223d251fce7f07e9e5f9e3732f9
Butt4cak3 <butt4cak3@gmail.com>
2017-07-25 13:22:30 +0200

    Got rid of the Main class
    This was originally ported over from Java. But now that it runs, we can get rid of it and make it more JavaScripty.

--- js/main.ts
+++ js/main.ts
@@ -1,33 +1,23 @@
-class Main {
-	private static readonly WIDTH = 101;
-	private static readonly HEIGHT = 101;
-	private static readonly START = new Point(0, 0);
-	private static readonly GOAL = new Point(Main.WIDTH - 1, Main.HEIGHT - 1);
-	private static readonly SCALE = 5;
-
-	public static main(): void {
-		let canvas = document.createElement("canvas"),
-			mazeObj: maze.Maze,
-			generator = new maze.generator.DfsMazeGenerator(),
-			solver = new maze.solver.DfsMazeSolver(),
-			drawer = new maze.drawer.CanvasMazeDrawer(canvas);
-
-		document.body.appendChild(canvas);
-
-		canvas.width = (Main.WIDTH + 2) * Main.SCALE;
-		canvas.height = (Main.HEIGHT + 2) * Main.SCALE;
-
-		mazeObj = generator.init(Main.WIDTH, Main.HEIGHT, Main.START, Main.GOAL);
-		solver.init(mazeObj);
-		drawer.draw(mazeObj, Main.SCALE);
-		drawer.attach(mazeObj, Main.SCALE);
-
-		animate(generator, () => {
-			animate(solver);
-		});
-	}
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-	Main.main();
+	const width = 101,
+		  height = 101,
+		  start = new Point(0, 0),
+		  goal = new Point(width - 1, height - 1),
+		  scale = 5,
+		  canvas = document.createElement("canvas"),
+		  generator = new maze.generator.DfsMazeGenerator(),
+		  solver = new maze.solver.DfsMazeSolver(),
+		  drawer = new maze.drawer.CanvasMazeDrawer(canvas);
+
+	document.body.appendChild(canvas);
+
+	canvas.width = (width + 2) * scale;
+	canvas.height = (height + 2) * scale;
+
+	const mazeObj = generator.init(width, height, start, goal);
+	solver.init(mazeObj);
+	drawer.draw(mazeObj, scale);
+	drawer.attach(mazeObj, scale);
+
+	animate(generator, () => animate(solver));
 });
